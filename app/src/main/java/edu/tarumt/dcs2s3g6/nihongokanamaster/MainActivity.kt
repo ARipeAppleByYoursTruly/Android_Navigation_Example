73c927d35e46a4b067aa49a1ececae1a76ad8ab6
ARipeAppleByYoursTruly <83992779+ARipeAppleByYoursTruly@users.noreply.github.com>
2023-04-28 15:51:44 +0800

    Initial commit

---
+++ app/src/main/java/edu/tarumt/dcs2s3g6/nihongokanamaster/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.tarumt.dcs2s3g6.nihongokanamaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
